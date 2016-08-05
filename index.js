4681b42743207252b6715c35fa7ad57bb1f475d6
Ethan Weeks <ethanweeks@ymail.com>
2016-08-05 17:56:17 -0600

    initial commit + index js is pulling lap time pdf and copying to json

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var fs = require('fs'),
+	download = require('download'),
+	PDFParser = require('./node_modules/pdf2json/PDFParser');
+
+
+
+// Only need to run once 
+// PDFs will be stored in this project directory
+// no need to execute on ever test 
+// PDF only get added by AMA after races which ever weekend during summer
+function fetchLapTimesPDF() {
+	var promises = [];
+
+	for(var i = 1605; i < 1660; i += 5) {
+		for(var n = 1; n < 3; n++) {
+			promises.push("http://americanmotocrossresults.com/xml/MX/events/M"+ i +"/M"+ n +"F1RID.pdf");
+			promises.push("http://americanmotocrossresults.com/xml/MX/events/M"+ i +"/M"+ n +"F2RID.pdf");
+		}
+	}
+
+	Promise.all(
+		promises.map(function(pdfLink, index){
+			download(pdfLink).then(function(dataPDF){
+					fs.writeFile('laptimes/moto'+index+'.pdf', dataPDF);
+				});
+		})).then(function(){
+			console.log("Lap Time PDFs have been downloaded successfully.");
+
+		}).catch(function(reject) {
+			console.log("Download ended in Error: "+ reject);
+		});
+}
+
+
+
+// Reads the lap time directory &&
+// calls the copy function on each pdf file
+function readLapTimesDir() {
+	fs.readdir('laptimes', function(err, motoFilesArr) {
+		if(err) throw err;
+		motoFilesArr.forEach(function(curr, index) {
+			copyPDFToJSON(curr);
+		});
+	});
+}
+
+
+
+// Makes of copy of the pdf in JSON format
+// Stores it in local laptimes directory
+function copyPDFToJSON(filePath) {
+
+	var pdfParser = new PDFParser();
+
+	pdfParser.on("pdfParser_dataError", function(errData){ 
+		console.error(errData.parserError); 
+	});
+	pdfParser.on("pdfParser_dataReady", function(pdfData){
+    fs.writeFile("./laptimes/"+filePath+".json", JSON.stringify(pdfData));
+	});
+
+	pdfParser.loadPDF("./laptimes/"+filePath);
+
+}
+
