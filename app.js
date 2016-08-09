d479bd0b461ca805cf74d5881829061915af3f69
Ethan Weeks <ethanweeks@ymail.com>
2016-08-08 20:17:03 -0600

    updated with fetch and pdf coverter being call per file and not loop through directory/ streamstring error

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 
 let fs = require('fs'),
 	download = require('download'),
-  PDFParser = require('./node_modules/pdf2json/PDFParser');
+  PDFParser = require('./node_modules/pdf2json/pdfparser');
+
+
 
  function urlArr() {
- 		const urlsToGet = [];
+ 	const urlsToGet = [];
 
 	for(var i = 1605; i < 1660; i += 5) {
 		for(var n = 1; n < 3; n++) {
@@ -14,30 +16,35 @@ let fs = require('fs'),
 		}
 	}
 	return urlsToGet;
- };
+ }
+
 
 function fetchLapTimesPDF() {
 
 	Promise.all(
-		urlArr().map(function(pdfLink){
-			download(pdfLink).then((pdf, index) => {
-					fs.writeFile('laptimes/moto'+index+'.pdf', pdf);
+		urlArr().map((pdfLink, index) => {
+			download(pdfLink).then(pdf => {
+					let pdfFilePath = 'laptimes/moto'+index+'.pdf';
+					let jsonFilePath = 'lap-times-json/moto'+index+'.json';
+					fs.writeFile(pdfFilePath, pdf, (err) => {
+						if(err) console.error("PDF write file threw "+ err);
+						pdfTojson(pdfFilePath, jsonFilePath);
+					});				
 				});
 		})).then(() => {
-			console.log("Lap Time PDFs have been downloaded successfully.");	
-
+			console.log("Lap Time PDFs have been downloaded successfully.");
 		}).catch(reject => {
 			console.error("Download ended in Error: "+ reject);
 		});
 }
 
 
-function pdfTojson(pdf) {
+function pdfTojson(pdfFilePath, jsonFilePath) {
 
-	let inputStream = fs.createReadStream(pdf);
-  let outputStream = fs.createWriteStream("./stuff.json");
+		let inputStream = fs.createReadStream(pdfFilePath);
+  	let outputStream = fs.createWriteStream(jsonFilePath);
 
-  inputStream.pipe(new PDFParser()).pipe(new StringifyStream()).pipe(outputStream);
+  	return inputStream.pipe(new PDFParser()).pipe(new StringifyStream()).pipe(outputStream);
 
 }
   
